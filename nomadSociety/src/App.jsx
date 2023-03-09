60882bc7a458d3bed9eb20bae4273c97c38dfefb
Alberto <albertsg.86@gmail.com>
2023-03-09 23:50:00 +0100

    w

--- nomadSociety/src/App.jsx
+++ nomadSociety/src/App.jsx
@@ -1,12 +1,23 @@
 import { Routes } from './Components/Routes/Routes'
 import { BrowserRouter } from "react-router-dom"
 import './App.scss'
+import { NavBar } from './Components/NavBar/NavBar'
+import { Header } from './Components/Header/Header'
+import { UsersProvider } from './context/UsersState'
 
 export const App = () => {
 
   return (
-    <BrowserRouter>
-      <Routes/>
-    </BrowserRouter>
+    <div className='body'>
+      <UsersProvider>
+        <BrowserRouter>
+        <NavBar className='navbar'/>
+          <div className='header-container'>
+            <Header className='header'/>
+            <Routes className='content'/>
+          </div>
+        </BrowserRouter>
+      </UsersProvider>
+    </div>
   )
 }
