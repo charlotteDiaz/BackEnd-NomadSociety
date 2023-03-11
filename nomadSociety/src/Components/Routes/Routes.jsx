cc696577169abbe16efc42a0fd7c69845c914a1e
Alberto <albertsg.86@gmail.com>
2023-03-11 10:18:26 +0100

    k

--- nomadSociety/src/Components/Routes/Routes.jsx
+++ nomadSociety/src/Components/Routes/Routes.jsx
@@ -1,27 +1,37 @@
 import { useRoutes } from 'react-router-dom';
+<<<<<<< HEAD
+import { PrivateZone } from '../../guards/PrivateZone';
 import { Register } from '../Auth/Register/Register';
+=======
+import { Login } from '../Auth/Login/Login';
+>>>>>>> 4ad06e255a691e088594aadce86f7311f0eac65e
 import { Content } from '../Content/Content';
-import { Profile } from '../Profile/Profile';
-
-
+// import { Profile } from '../Profile/Profile';
 
 export const Routes = () => {
   return useRoutes(
 
         [
               {
-                element: <Content/>,
+<<<<<<< HEAD
+                element:<Content/> ,
                 path: '/'
               },
               {
-                element: <Profile/>,
+                element:<PrivateZone><Profile/></PrivateZone> ,
                 path: '/profile'
+              }
+=======
+                element: <Login/>,
+                path: '/'
               },
               {
-                element: <Register/>,
-                path: '/register'
+                element: <Content/>,
+                path: '/home'
               },
+>>>>>>> 4ad06e255a691e088594aadce86f7311f0eac65e
         ]
         );
 
 }
+//f
