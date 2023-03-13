8de21bab02c1f9b1e1abc764e83d91caa6961d33
Alberto <albertsg.86@gmail.com>
2023-03-13 23:49:19 +0100

    d

--- nomadSociety/src/Components/Content/Content.jsx
+++ nomadSociety/src/Components/Content/Content.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router';
 import './Content.scss'
 
 export const Content = () => {
   
   return (
     <div className='content'>
-
+      hola que tal
     </div>
   )
 }
