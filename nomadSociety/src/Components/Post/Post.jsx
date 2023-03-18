55d6b39e60484395855a29f30e6f1b968fc02696
charlotteDiaz <33845697+charlotteDiaz@users.noreply.github.com>
2023-03-18 13:25:19 +0100

    changes

--- nomadSociety/src/Components/Post/Post.jsx
+++ nomadSociety/src/Components/Post/Post.jsx
@@ -1,9 +1,18 @@
 import { Card } from 'antd';
+import TimeAgo from 'react-timeago'
 import './Post.scss'
 
 function Post(props){
   const { userName, profilePhoto, postTitle, postContent, imgContent, createdAt } = props;
 
+  function dateFormatter(value,unit){
+    if (unit === 'second' && value < 15) return "just now";
+    if (unit === "second") return "few seconds ago";
+    if (unit  === "minute") return `${value} ${value === 1 ? "minute" : "minutes"} ago`;
+    if (unit  === "hour") return `${value} ${value === 1 ? "hour" : "hours"} ago`;
+    if (unit  === "day") return `${value} ${value === 1 ? "day" : "days"} ago`;
+    console.log(value,unit);
+  }
 
   return(
     <Card size='large' style={{ width: 800, backgroundColor: '#f0f0f0'}}>
@@ -11,15 +20,15 @@ function Post(props){
         <div className='userInfo'>
           <img src={ profilePhoto } className='imgProfile'/>
           <div className='userName'>{ userName }</div>
+          <div><TimeAgo date={createdAt} live='false' formatter={(unit,value) => dateFormatter(unit,value)}/></div>
         </div>
-        <div>
-          { createdAt }
-        </div>
-        <div className='title'>
-          {postTitle}
-        </div>
-        <div>
-          { postContent }
+        <div className='postInfo'>
+          <div className='title'>
+            {postTitle}
+          </div>
+          <div>
+            { postContent }
+          </div>
         </div>
       </div>
     </Card>
