0b1e2c570436c578f1d370856cf49da8628efc34
Alberto <albertsg.86@gmail.com>
2023-03-19 23:50:52 +0100

    s

--- nomadSociety/src/main.jsx
+++ nomadSociety/src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { CommentsProvider } from './context/comments/CommentsState'
 import { UsersProvider } from './context/UsersState'
 import { PostProvider } from './context/PostContext/PostState'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UsersProvider>
+<<<<<<< HEAD
     <CommentsProvider>
       <PostProvider>
         <App />
       </PostProvider>
     </CommentsProvider>
+=======
+      <App />
+>>>>>>> 9846aa5b57b5eeb043a07dad46dce6ded4c743b5
   </UsersProvider>
 )
