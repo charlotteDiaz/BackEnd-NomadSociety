632413679ad17ea236e77b851709afdb02f956a6
Alberto <albertsg.86@gmail.com>
2023-03-21 16:32:42 +0100

    s

--- nomadSociety/src/Components/Content/Content.jsx
+++ nomadSociety/src/Components/Content/Content.jsx
@@ -1,17 +1,15 @@
+
 import './Content.scss'
 import React, { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../context/UsersState";
-import { LikeButton } from '../LikeButton/LikeButton';
-import { PostComponent } from '../PostComponent/PostComponent';
 export const Content = () => {
   const { getUserInfo, user, deleteUser, logOut, reset } = useContext(GlobalContext);
   useEffect(()=>{
     getUserInfo()
-
   },[])
   return (
     <div className='content'>
-      <PostComponent />
+      hola que tal
     </div>
   )
 }
