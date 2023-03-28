a54398af8e23ab673ed538714ecee5317247290a
Alberto <albertsg.86@gmail.com>
2023-03-28 18:27:47 +0200

    s

--- nomadSociety/src/Components/CreatePost/BadLanguage 2.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './badLanguage.scss'
-
-export const BadLanguage = () => {
-  return (
-    <div className='badLanguage'>En tu comentario hay lenguaje ofensivo. Por favor, modifícalo</div>
-  )
-}
